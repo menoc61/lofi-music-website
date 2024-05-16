24a1c6671b7299ddf6d8b8f3cc72aab9c3140901
GILLE MOMENI <gillemomeni@gmail.com>
2024-05-17 01:31:59 +0300

    fetch github stars

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,5 +1,9 @@
-export const CONSTANTS  = {
-    'AUTHOR': 'Gilles Momeni',
-    'AUTHOR_GITHUB_LINK': 'https://github.com/menoc61',
-    'AUTHOR_PORTFOLIO_LINK': 'https://gilles-momeni.vercel.app/',
-}
+export const CONSTANTS = {
+  AUTHOR: "Gilles Momeni",
+  AUTHOR_GITHUB_LINK: "https://github.com/menoc61",
+  PROJECT_GITHUB_LINK: "https://github.com/menoc61/lofi-music-website",
+  AUTHOR_PORTFOLIO_LINK: "https://gilles-momeni.vercel.app/",
+  GITHUB_API_BASE_URL: "https://api.github.com",
+  REPO_OWNER: "menoc61",
+  REPO_NAME: "lofi-music-website",
+};
--- src/layout/Header/index.tsx
+++ src/layout/Header/index.tsx
@@ -1,4 +1,4 @@
-import  { useState } from "react";
+import  { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { changeDayNight } from "../../store/slice/modeSlice";
 import "./styles.scss";
@@ -6,24 +6,41 @@ import { Link } from "react-router-dom";
 import DarkLightSwitch from "../../components/DarkLightSwitch";
 import { CONSTANTS } from "../../constants/constants";
 import { RootState } from "../../store/store";
-
+import { Spinner } from "react-bootstrap";
+import axios from "axios";
 export interface IDarkLightSwitchProps {
   theme: string;
 }
 
 const Header = () => {
   const [fullscreen, setFullscreen] = useState<boolean>(false);
+  const [stars, setStars] = useState<number | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
   const daynight = useSelector((state: RootState) => state.mode);
   const dispatch = useDispatch();
   const { mode } = daynight;
 
+  useEffect(() => {
+    const fetchStars = async () => {
+      try {
+        const response = await axios.get(`https://api.github.com/repos/${CONSTANTS.REPO_OWNER}/${CONSTANTS.REPO_NAME}`);
+        const { stargazers_count } = response.data;
+        setStars(stargazers_count);
+      } catch (error) {
+        console.error("Failed to fetch star count:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchStars();
+  }, []);
   const daynightHandler = () => {
     dispatch(changeDayNight());
   };
 const addStarHandler = () => {
-  console.log('star');
+  console.log('star');  
 }
-const stars : number = 3;
+
   const fullscreenHandler = () => {
     if (!fullscreen) {
       document.documentElement.requestFullscreen();
@@ -45,11 +62,16 @@ const stars : number = 3;
           <i className='fab fa-github'></i>
           <span>GitHub</span>
         </a>
-        <div className='github-stars'>
-          <span>{stars} Stars</span>
-          <button onClick={addStarHandler} className='star-btn'>
-            <i className='fas fa-star'></i> Star
-          </button>
+        <div className=" d-flex align-items-center">
+          {loading ? (
+            <Spinner size="sm" />
+          ) : (
+            <div onClick={addStarHandler} className='button'>
+             <div className='icon'>
+             {stars !== null ? stars : "N/A"}<i className="fas fa-star"></i>
+             </div>
+            </div>
+          )}
         </div>
         <a target='_blank' rel='noreferrer' href={CONSTANTS.AUTHOR_PORTFOLIO_LINK}>
           <i className='fas fa-globe'></i>
